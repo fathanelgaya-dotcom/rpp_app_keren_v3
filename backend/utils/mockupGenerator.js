65a26a6fe48f113fb392e552d8dad4aea4969253
Fathan Elgaya <fathanelgaya@gmail.com>
2025-10-28 16:51:12 +0700

    update exportController - tambah tabel penilaian diri di lembar kerja

--- backend/utils/mockupGenerator.js
+++ backend/utils/mockupGenerator.js
@@ -626,6 +626,9 @@ const finalDigital = dynamicDigital;
     let clean = x.replace(/\*\*/g, "").trim();
     let titleOnly = clean.split(":")[0].trim();
     return `Keterlibatan dalam kegiatan: ${titleOnly}`;
-    }),
-  },
+    },
 };
+
+} // ⬅️ ini penutup RETURN {...}
+
+} 
