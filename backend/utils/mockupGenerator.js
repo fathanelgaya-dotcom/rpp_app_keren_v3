93112c561c2accb123cd8b54a25233288772d68f
Fathan Elgaya <fathanelgaya@gmail.com>
2025-10-28 14:22:33 +0700

    update exportController - tambah tabel penilaian diri di lembar kerja

--- backend/utils/mockupGenerator.js
+++ backend/utils/mockupGenerator.js
@@ -612,14 +612,15 @@ const finalDigital = dynamicDigital;
       tujuan: `Memastikan target ketercapaian ${cp}`,
       tugas: `Kerjakan soal/tugas dengan ${profilLulusan}`,
       urutan_kerja: "Catatlah setiap tahapan kegiatan pembelajaran yang kamu lakukan, berilah penanda pada poin penting yang dicapai", 
-      rubrik: ...,
-        tabel_penilaian_diri: {
+      rubrik: {
+        tabel_penilaian_diri: [
           instruksi: `Isilah tabel penilaian diri berikut berdasarkan keterlibatan Anda dalam setiap tahapan kegiatan pada *Kegiatan Inti*, gunakan skala 1–4 secara jujur.`,
           skala: "1-4",
           indikator: langkah
             .filter((x) => !x.startsWith("**"))
             .map((x, i) => `Keterlibatan dalam kegiatan: ${x}`)
-        }
+        ]
+      }
     }
   };
 }
